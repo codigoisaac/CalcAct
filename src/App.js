3796dca24220ba8bd64a84309b7ca793c98ab768
Isaac Muniz <codigoisaac@protonmail.com>
2021-04-21 22:55:13 -0300

    Bring unfinished app from Codepen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0],
+  ops = ["/", "x", "-", "+", "="];
+
+class App extends React.Component {
+  state = {
+    lastPressed: undefined,
+    currentNumber: "0",
+    prevNumber: undefined,
+  };
+
+  onClick = (e) => {
+    const { innerText } = e.target,
+      { lastPressed, currentNumber, prevNumber } = this.state;
+
+    if (!Number.isNaN(Number(innerText))) {
+      // if is not a number
+      if (currentNumber === "0") {
+        this.setState({ currentNumber: innerText });
+      } else {
+        this.setState({ currentNumber: currentNumber + innerText });
+      }
+    }
+
+    switch (innerText) {
+      case "C":
+        this.setState({ currentNumber: "0", prevNumber: undefined });
+        break;
+    }
+
+    this.setState({ lastPressed: innerText });
+  };
+
+  render() {
+    const { currentNumber } = this.state;
+
+    return (
+      <main>
+        <header>{currentNumber}</header>
+        <div id="nums-container">
+          <button id="clear" onClick={this.onClick}>
+            C
+          </button>
+          {nums.map((num) => (
+            <button
+              className={`number ${num === 0 && "big-h"}`}
+              onClick={this.onClick}
+            >
+              {num}
+            </button>
+          ))}
+          <button id="decimal" onClick={this.onClick}>
+            .
+          </button>
+        </div>
+        <div id="ops-container">
+          {ops.map((op) => (
+            <button className="operator" key={op} onClick={this.onClick}>
+              {op}
+            </button>
+          ))}
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
