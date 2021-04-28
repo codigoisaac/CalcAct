d9239deee12b89c6527223f83678ec24e0c67d37
Isaac Muniz <codigoisaac@protonmail.com>
2021-04-28 15:38:14 -0300

    New logic, positive operations done

--- src/App.js
+++ src/App.js
@@ -2,56 +2,74 @@ import "./index.css";
 import React from "react";
 
 const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0],
+  numIds = [
+    "seven",
+    "eight",
+    "nine",
+    "four",
+    "five",
+    "six",
+    "one",
+    "two",
+    "three",
+    "zero",
+  ],
   ops = ["/", "*", "-", "+", "="],
   opIds = ["divide", "multiply", "subtract", "add", "equals"];
 
 class App extends React.Component {
   state = {
     currentNumber: "0",
-    store: undefined,
-    operation: undefined,
-    lastPressed: undefined,
+    showingResult: false,
   };
 
   onClick = (e) => {
-    const { store, currentNumber, lastPressed } = this.state,
+    const { currentNumber, showingResult } = this.state,
       { innerText } = e.target;
 
     switch (innerText) {
-      case "C":
+      case "C": {
         this.setState({
           currentNumber: "0",
-          store: undefined,
+          showingResult: false,
         });
         break;
+      }
 
-      case "=":
-        const evaluated = eval(store);
-        this.setState({ currentNumber: evaluated, store: evaluated });
+      case ".": {
+        if (!currentNumber.includes(".")) {
+          this.setState({ currentNumber: currentNumber + innerText });
+        }
         break;
+      }
 
-      default:
-        if (nums.includes(innerText)) {
+      case "=": {
+        const evaluated = eval(currentNumber).toString();
+        this.setState({ currentNumber: evaluated, showingResult: true });
+        break;
+      }
+
+      default: {
+        if (nums.includes(parseInt(innerText))) {
           // pressed a number
+          if (currentNumber === "0" || showingResult === true) {
+            this.setState({ currentNumber: innerText, showingResult: false });
+          } else {
+            this.setState({ currentNumber: currentNumber + innerText });
+          }
         } else {
           // pressed a operator
-          if (ops.includes(lastPressed)) {
-            // last pressed was already an operator
+          if (ops.includes(currentNumber.slice(-1))) {
+            // operator followed by operator
             this.setState({
-              currentNumber: currentNumber.slice(0, -1),
+              currentNumber: currentNumber.slice(0, -1) + innerText,
             });
-            alert("last = op");
           } else {
-            // last pressed was a number
+            this.setState({ currentNumber: currentNumber + innerText });
           }
+          this.setState({ showingResult: false });
         }
-      // const actual =
-      //   currentNumber === "0" ? innerText : currentNumber + innerText;
-      // this.setState({
-      //   store: actual,
-      //   currentNumber: actual,
-      //   lastPressed: innerText,
-      // });
+      }
     }
   };
 
@@ -62,23 +80,28 @@ class App extends React.Component {
       <main>
         <small>{store}</small>
         <header id="display">{currentNumber}</header>
+
         <div id="nums-container">
           <button id="clear" onClick={this.onClick}>
             C
           </button>
+
           {nums.map((num) => (
             <button
-              className={`number ${num === 0 && "big-h"}`}
+              className={`number${num === 0 ? " big-h" : ""}`}
               onClick={this.onClick}
               key={num}
+              id={numIds[nums.indexOf(num)]}
             >
               {num}
             </button>
           ))}
+
           <button id="decimal" onClick={this.onClick}>
             .
           </button>
         </div>
+
         <div id="ops-container">
           {ops.map((op) => (
             <button
